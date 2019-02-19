2262bc1da1216bc6d941a8004a5fb89fbae57538
Айнур-ПК\Айнур <felix-xilef@mail.ru>
2019-02-19 11:43:14 +0300

    Добавляю товары в корзину

--- js/script.js
+++ js/script.js
@@ -1,23 +1,46 @@
-const cartWrapper = document.querySelector('.cart__wrapper'),
-      cart = document.querySelector('.cart'),
-      close = document.querySelector('.cart__close'),
-      open = document.querySelector('#cart'),
-      goodsBtn = document.querySelectorAll('.goods__btn'),
-      products = document.querySelectorAll('.goods__item'),
-      confirm = document.querySelector('.confirm'),
-      badge = document.querySelector('.nav__badge'),
-      totalCost = document.querySelector('.cart__total > span'),
-      titles = document.querySelectorAll('.goods__title');
+window.addEventListener('DOMContentLoaded', function() {
 
-var openCart = function() {
-    cart.style.display = 'block';
-    document.body.style.overflow = 'hidden';
-};
+    const cartWrapper = document.querySelector('.cart__wrapper'),
+        cart = document.querySelector('.cart'),
+        close = document.querySelector('.cart__close'),
+        open = document.querySelector('#cart'),
+        goodsBtn = document.querySelectorAll('.goods__btn'),
+        products = document.querySelectorAll('.goods__item'),
+        confirm = document.querySelector('.confirm'),
+        badge = document.querySelector('.nav__badge'),
+        totalCost = document.querySelector('.cart__total > span'),
+        titles = document.querySelectorAll('.goods__title');
 
-var closeCart = function() {
-    cart.style.display = 'none';
-    document.body.style.overflow = '';
-};
+    var openCart = function() {
+        cart.style.display = 'block';
+        document.body.style.overflow = 'hidden';
+    };
 
-open.addEventListener('click', openCart);
-close.addEventListener('click', closeCart);
+    var closeCart = function() {
+        cart.style.display = 'none';
+        document.body.style.overflow = '';
+    };
+
+    open.addEventListener('click', openCart);
+    close.addEventListener('click', closeCart);
+
+    goodsBtn.forEach(function(btn, i) {
+        btn.addEventListener('click', function() {
+            let item = products[i].cloneNode(true),
+                trigger = item.querySelector('button'),
+                removeBtn = document.createElement('div'),
+                empty = cartWrapper.querySelector('.empty');
+            
+            trigger.remove();
+
+            removeBtn.classList.add('goods__item-remove');
+            removeBtn.innerHTML = '&times';
+            item.appendChild(removeBtn);
+
+            cartWrapper.appendChild(item);
+            if (empty) {
+                empty.remove();
+            }
+        });
+    });
+});
